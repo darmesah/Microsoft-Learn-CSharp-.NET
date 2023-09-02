6603f59c537bd092a5973c873eeb35be6c3ba802
Damisa Alex <damisa.alex@gmail.com>
2023-09-02 21:08:57 +0100

    updated learning

---
+++ 01 Getting Started with C#/03 Part 4/02 Convert data types/Program.cs
@@ -0,0 +1,108 @@
+﻿// int first = 2;
+// string second = "4";
+// int result = first + second;
+// Console.WriteLine(result);
+
+// // int first = 2;
+// // string second = "4";
+// // string result = first + second;
+// // Console.WriteLine(result);
+
+
+
+// int myInt = 3;
+// Console.WriteLine($"int: {myInt}");
+
+// decimal myDecimal = myInt;
+// Console.WriteLine($"decimal: {myDecimal}");
+
+
+// decimal myDecimal = 3.14m;
+// Console.WriteLine($"decimal: {myDecimal}");
+
+// int myInt = (int)myDecimal;
+// Console.WriteLine($"int: {myInt}");
+
+
+// // Convert a string to an int using the Parse() helper method
+
+// string first = "5";
+// string second = "7";
+// int sum = int.Parse(first) + int.Parse(second);
+// Console.WriteLine(sum);
+
+
+// // Convert a string to a int using the Convert class
+// string first = "5";
+// string second = "7";
+// int result = Convert.ToInt32(first) * Convert.ToInt32(second);
+// Console.WriteLine(result);
+
+
+// // Compare casting and converting a decimal into an int
+// int value = (int)1.5m; // casting truncates
+// Console.WriteLine(value);
+
+// int value2 = Convert.ToInt32(1.5m); // converting rounds up
+// Console.WriteLine(value2);
+
+
+
+// // Examine the TryParse() method
+// string value = "100";
+// int result = 0;
+
+// if (int.TryParse(value, out result))
+// {
+//     Console.WriteLine("Mesurement: " + result);
+// }
+// else
+// {
+//     Console.WriteLine("Unable to report the mesurement");
+// }
+
+// Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+
+
+
+// // Exercise - Complete a challenge to combine string array values as strings and as integers
+
+// string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+
+// string message = "";
+// decimal total = 0m;
+
+// foreach (var value in values)
+// {
+//     if (decimal.TryParse(value, out decimal num))
+//     {
+//         total += num;
+//     }
+//     else
+//     {
+//         message += value;
+//     }
+// }
+
+// Console.WriteLine("Message: " + message);
+// Console.WriteLine("Total: " + total);
+
+
+
+// Exercise - Complete a challenge to output math operations as specific number types
+int value1 = 12;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
+
+// Your code here to set result1
+// Hint: You need to round the result to nearest integer (don't just truncate)
+int result1 = Convert.ToInt32(value1 / value2);
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+// Your code here to set result2
+decimal result2 = value2 / (decimal)value3;
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+// Your code here to set result3
+float result3 = value3 / value1;
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
---
+++ 01 Getting Started with C#/03 Part 4/03  Perform operations on arrays/Program.cs
@@ -0,0 +1,109 @@
+﻿// string[] pallets = { "B14", "A11", "B12", "A13" };
+
+// // SORT
+// Console.WriteLine("Sorted...");
+// Array.Sort(pallets);
+
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine("--" + pallet);
+// }
+
+// Console.WriteLine("");
+
+// // REVERSE
+// Console.WriteLine("Reversed...");
+// Array.Reverse(pallets);
+
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine("--" + pallet);
+// }
+
+
+// string[] pallets = { "B14", "A11", "B12", "A13" };
+
+// // CLEAR
+// Console.WriteLine("Before: " + pallets[0]);
+// Array.Clear(pallets, 0, 2);
+// Console.WriteLine("After: " + pallets[0]);
+
+// Console.WriteLine("Clearing 2 ... count: " + pallets.Length);
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine("-- " + pallet);
+// }
+
+// Console.WriteLine("");
+
+// // RESIZE the array to add more elements
+// Array.Resize(ref pallets, 6);
+// Console.WriteLine("Resizing 6 ... count: " + pallets.Length);
+
+// pallets[4] = "CO1";
+// pallets[5] = "CO2";
+
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine("-- " + pallet);
+// }
+
+// // RESIZE the array to add more elements
+// Array.Resize(ref pallets, 3);
+// Console.WriteLine("Resizing 3 ... count: " + pallets.Length);
+
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine("-- " + pallet);
+// }
+
+
+
+// string value = "abc123";
+// char[] valueArray = value.ToCharArray();
+// Array.Reverse(valueArray);
+// // string result = new string(valueArray);
+// string result = String.Join(",", valueArray);
+// Console.WriteLine(result);
+
+// string[] items = result.Split(',');
+// foreach (string item in items)
+// {
+//     Console.WriteLine(item);
+// }
+
+
+// Reverse words in a sentence challenge
+// string pangram = "The quick brown fox jumps over the lazy dog";
+
+// string[] stringArray = pangram.Split(" ");
+
+// string myString = "";
+
+// foreach (var item in stringArray)
+// {
+//     char[] charArray = item.ToCharArray();
+//     Array.Reverse(charArray);
+//     string result = new string(charArray);
+//     myString += result + " ";
+// }
+
+// Console.WriteLine(myString);
+
+
+
+// Parse a string of orders, sort the orders and tag possible errors
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+
+string[] orderStreamArray = orderStream.Split(",");
+Array.Sort(orderStreamArray);
+
+for (int i = 0; i < orderStreamArray.Length; i++)
+{
+    if (orderStreamArray[i].Length != 4)
+    {
+        orderStreamArray[i] = orderStreamArray[i] + "\t - Error";
+    }
+
+    Console.WriteLine(orderStreamArray[i]);
+}
